2b8cff5a97e7c89fe1cc2787c222850015c3fcc7
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-06-25 19:01:47 +0900

    オプション登録のコードをリファクタリングしてみやすくしました。

--- quiz-app/app/Http/Controllers/QuizController.php
+++ quiz-app/app/Http/Controllers/QuizController.php
@@ -39,29 +39,61 @@ class QuizController extends Controller
         $quiz->save();
         // クイズIDをもとに選択肢（option）を登録する
         $quizId = $quiz->id;
-        $option1 = new Option();
-        $option1->quiz_id = $quiz->id;
-        $option1->content = $request->content1;
-        $option1->is_correct = $request->isCorrect1;
-        $option1->save();
 
-        $option2 = new Option();
-        $option2->quiz_id = $quiz->id;
-        $option2->content = $request->content2;
-        $option2->is_correct = $request->isCorrect2;
-        $option2->save();
+        $options = [
+            [
+                'quiz_id' => $quiz->id,
+                'content' => $request->content1,
+                'is_correct' => $request->isCorrect1
+            ],
+            [
+                'quiz_id' => $quiz->id,
+                'content' => $request->content2,
+                'is_correct' => $request->isCorrect2
+            ],
+            [
+                'quiz_id' => $quiz->id,
+                'content' => $request->content3,
+                'is_correct' => $request->isCorrect3
+            ],
+            [
+                'quiz_id' => $quiz->id,
+                'content' => $request->content4,
+                'is_correct' => $request->isCorrect4
+            ],
+        ];
 
-        $option3 = new Option();
-        $option3->quiz_id = $quiz->id;
-        $option3->content = $request->content3;
-        $option3->is_correct = $request->isCorrect3;
-        $option3->save();
+        foreach ($options as $option) {
+            $newOption = new Option();
+            $newOption->quiz_id = $option['quiz_id'];
+            $newOption->content = $option['content'];
+            $newOption->is_correct = $option['is_correct'];
+            $newOption->save();
+        }
 
-        $option4 = new Option();
-        $option4->quiz_id = $quiz->id;
-        $option4->content = $request->content4;
-        $option4->is_correct = $request->isCorrect4;
-        $option4->save();
+        // $option1 = new Option();
+        // $option1->quiz_id = $quiz->id;
+        // $option1->content = $request->content1;
+        // $option1->is_correct = $request->isCorrect1;
+        // $option1->save();
+
+        // $option2 = new Option();
+        // $option2->quiz_id = $quiz->id;
+        // $option2->content = $request->content2;
+        // $option2->is_correct = $request->isCorrect2;
+        // $option2->save();
+
+        // $option3 = new Option();
+        // $option3->quiz_id = $quiz->id;
+        // $option3->content = $request->content3;
+        // $option3->is_correct = $request->isCorrect3;
+        // $option3->save();
+
+        // $option4 = new Option();
+        // $option4->quiz_id = $quiz->id;
+        // $option4->content = $request->content4;
+        // $option4->is_correct = $request->isCorrect4;
+        // $option4->save();
 
         return redirect()->route('admin.categories.show', ['categoryID' =>$categoryID]);
     }
