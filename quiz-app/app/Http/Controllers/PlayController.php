fe7886cc1b2f5871ad2adb3b5f8f4015314b428e
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-06-27 21:44:26 +0900

    answerメソッドとisCorrectAnswerメソッドを実装しました。

--- quiz-app/app/Http/Controllers/PlayController.php
+++ quiz-app/app/Http/Controllers/PlayController.php
@@ -43,6 +43,32 @@ class PlayController extends Controller
     public function answer(Request $request, string $categoryId) {
         $quizId = $request->quizId;
         $optionId = $request->optionId;
+        $category = Category::with('quizzes.options')->findOrFail($categoryId);
+        $quiz = $category->quizzes()->firstWhere('id', $quizId);
+        $quizOptions =$quiz->options->toArray();
+        $result = $this->isCorrectAnswer($optionId, $quizOptions);
         return  view('play.answer');
+    }
+
+    // プレイヤーの回答が正解か不正解か判定
+    private function isCorrectAnswer(array $selectedOptions, array $quizOptions) {
+        // クイズの選択肢から正解の選択肢を抽出し、そのIDをすべて取得する。
+        $correctOptions = array_filter($quizOptions, function($option) {
+            return $option['is_correct'] === 1;
+        });
+        // プレイヤーが選んだ選択肢の個数と正解の選択肢の個数が一致するか判定す る
+            if (count($selectedOptions) !== count($correctOptions) )
+            {
+                return false;
+            }
+
+        // プレイヤーが選んだ選択肢のIDと正解のIDがすべて一致することを判定する
+        foreach ($selectedOptions as $selectedOption) {
+            if (!in_array($selectedOption, $correctOptions)) {
+                return false;
+            }
+        }
+        //正解であることを返す
+        return true;
     }
 }
