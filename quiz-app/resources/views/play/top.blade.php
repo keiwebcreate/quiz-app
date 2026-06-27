c1c2139ae11cb9b3964c77bcfd1ca71e0865771b
keiwebcreate <keiwebcreate20240704@gmail.com>
2026-06-27 18:38:41 +0900

    プレイヤートップページの見た目のHTMLを整えました。

--- quiz-app/resources/views/play/top.blade.php
+++ quiz-app/resources/views/play/top.blade.php
@@ -1,3 +1,42 @@
 <x-play-layout>
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap w-full mb-20">
+                <div class=" w-full mb-6 lg:mb-0">
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 text-center">カテゴリーを選択してください</h1>
+                    <div class="mx-auto h-1 w-20 bg-indigo-500 rounded"></div>
+                </div>
+            </div>
+            <div class="flex flex-wrap -m-4">
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="" class="bg-gray-100 p-6 rounded-lg block">
 
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー1</h2>
+                        <p class="leading-relaxed text-base">カテゴリー１の説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="" class="bg-gray-100 p-6 rounded-lg block">
+
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー２</h2>
+                        <p class="leading-relaxed text-base">カテゴリー２の説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="" class="bg-gray-100 p-6 rounded-lg block">
+
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー３</h2>
+                        <p class="leading-relaxed text-base">カテゴリー３の説明文</p>
+                    </a>
+                </div>
+                <div class="xl:w-1/4 md:w-1/2 p-4">
+                    <a href="" class="bg-gray-100 p-6 rounded-lg block">
+
+                        <h2 class="text-lg text-gray-900 font-medium title-font mb-4">カテゴリー４</h2>
+                        <p class="leading-relaxed text-base">カテゴリー４の説明文</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-play-layout>
